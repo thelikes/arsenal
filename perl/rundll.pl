dda7227afe759dbddd76de1f18e37925a689d5b5
thelikes <thelikes@github.com>
2021-08-17 20:31:59 -0400

    update perl dll runner poc

--- perl/rundll.pl
+++ perl/rundll.pl
@@ -3,7 +3,7 @@ use Win32::API;
 # rundll32.pl - load dll with perl
 # @thelikes_
 # help: https://stackoverflow.com/questions/45059300/win32api-giving-wrong-prototype-error
-# sample dll: https://github.com/thelikes/arsenal/blob/main/c/exec_dll.c
+# sample dll: https://github.com/thelikes/arsenal/blob/main/c/exec_dll-2.c
 
 $function = Win32::API->new("c:\\payloads\\exec_dll\\output\\exec_dll64.dll", "DllMain",[ 'N', 'P', 'P' ], 'N', '__cdecl');
 
